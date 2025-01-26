fc4d81e1c5403ab18a780f7989db1baff2a213ce
amenibelhaj <blameni073@gmail.com>
2025-01-26 23:05:27 +0200

    integers

--- python-test_driven_development/0-add_integer.py
+++ python-test_driven_development/0-add_integer.py
@@ -1,21 +1,44 @@
 #!/usr/bin/python3
+#!/usr/bin/python3
+"""
+This module provides a function to add two integers.
+
+Examples:
+    >>> add_integer(1, 2)
+    3
+    >>> add_integer(100, -2)
+    98
+    >>> add_integer(2)
+    100
+    >>> add_integer(100.3, -2)
+    98
+    >>> add_integer(4, "School")
+    Traceback (most recent call last):
+        ...
+    TypeError: b must be an integer
+    >>> add_integer(None)
+    Traceback (most recent call last):
+        ...
+    TypeError: a must be an integer
+"""
+
+
 def add_integer(a, b=98):
     """
-    Adds two integers (or floats) and returns the result as an integer.
+    Adds two integers or floats, rounding them to the nearest integer.
 
     Args:
-        a: The first value. Can be an integer or float.
-        b: The second value. Can be an integer or float. Default is 98.
-
-    Raises:
-        TypeError: If either `a` or `b` is not an integer or float.
+    a (int, float): The first integer or float.
+    b (int, float): The second integer or float. Default is 98.
 
     Returns:
-        int: The sum of `a` and `b` as an integer.
-    """
-    if not isinstance(a, (int, float)):
-        raise TypeError("a must be an integer")
-    if not isinstance(b, (int, float)):
-        raise TypeError("b must be an integer")
+    int: The sum of a and b after rounding them.
 
-    return int(a) + int(b)
+    Raises:
+    TypeError: If a or b is not an integer or float.
+    """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    if type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return round(a) + round(b)
