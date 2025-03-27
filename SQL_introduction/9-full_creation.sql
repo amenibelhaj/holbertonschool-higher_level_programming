2ec5339524d106c678e3afbea073a78a03d4a649
amenibelhaj <blameni073@gmail.com>
2025-03-27 10:22:32 +0000

    full creation

---
+++ SQL_introduction/9-full_creation.sql
@@ -0,0 +1,16 @@
+-- This script creates the second_table if it doesn't exist and inserts rows into it.
+
+-- Create the table if it does not already exist
+CREATE TABLE IF NOT EXISTS second_table (
+  id INT,
+  name VARCHAR(256),
+  score INT
+);
+
+-- Insert multiple rows into second_table
+INSERT INTO second_table (id, name, score)
+VALUES
+  (1, 'John', 10),
+  (2, 'Alex', 3),
+  (3, 'Bob', 14),
+  (4, 'George', 8);
