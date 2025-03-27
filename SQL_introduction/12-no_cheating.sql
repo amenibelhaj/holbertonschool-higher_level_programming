a07ffb64153a5b291a29ac6edf880e5ea233361d
amenibelhaj <blameni073@gmail.com>
2025-03-27 10:35:19 +0000

    updating the score

--- SQL_introduction/12-no_cheating.sql
+++ SQL_introduction/12-no_cheating.sql
@@ -1,4 +1,4 @@
-o-- This script updates the score of Bob to 10 in the second_table
+-- This script updates the score of Bob to 10 in the second_table
 UPDATE second_table
 SET score = 10
 WHERE name = 'Bob';
