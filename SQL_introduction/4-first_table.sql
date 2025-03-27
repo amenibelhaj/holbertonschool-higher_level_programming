8793244656c8adcd344afe0f3a5b056797bfe60b
amenibelhaj <blameni073@gmail.com>
2025-03-27 10:05:46 +0000

    first table in database

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,6 @@
+-- This script creates the table 'first_table' in the specified database if it doesn't already exist.
+
+CREATE TABLE IF NOT EXISTS first_table (
+    id INT,
+    name VARCHAR(256)
+);
